eaa5be6169d3bdac7845d468660e15602e18cde6
pyfox <vincentbriffautcintrat@gmail.com>
2024-03-05 20:58:34 +0100

    Add Master and Slave scripts and supporting IDE files
    This commit introduces two new Python files, Master.py and Slave.py, which appear to set up a server and client respectively for some data processing operation. In tandem with these, several IDE (in this case, presumably PyCharm) configuration files have been added to support the development of these and future Python files.

---
+++ Master.py
@@ -0,0 +1,45 @@
+import flask
+import requests
+import itertools
+slaves = ["127.0.0.1:5001"]
+app = flask.Flask(__name__)
+
+
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+
+
+def listener(port):
+    app.run(port=port)
+
+
+def makeEmIter(it, to):
+    it = list(split(it, len(slaves)))
+    for v, i in enumerate(slaves):
+        req = requests.post(f"http://{i}/iter", json={"it": it[v], "to": to})
+        if req.status_code != 200:
+            print(f"ERROR ON {i}")
+    print("slaves started")
+
+
+@app.route('/receive', methods=['POST'])
+def receiver():
+    print(flask.request.data)
+    return "200"
+
+
+def makeIt():
+    string = "abcdefghijklmnopqrstuvwxyz"
+    it = list(itertools.product(string, repeat=5))
+    return it
+
+
+@app.route('/start')
+def start():
+    makeEmIter(makeIt(), to="abcde")
+    return "oui"
+
+if __name__ == '__main__':
+    listener(5000)
+
---
+++ Slave.py
@@ -0,0 +1,34 @@
+import flask
+import requests
+import itertools
+import json
+app = flask.Flask(__name__)
+
+
+
+def listener(port):
+    app.run(port=port)
+
+
+@app.route('/iter', methods=['POST'])
+def iter():
+    process(flask.request)
+    return "200"
+
+
+def process(req):
+    data = req.json
+    print(req.remote_addr)
+    it = data["it"]
+    to = data["to"]
+    print(to)
+    for i in it:
+        if ''.join(i) == to:
+            print(''.join(i))
+            req = requests.post(f"http://{req.remote_addr}:5000/receive", data=''.join(i))
+            print(req.status_code)
+            return True
+    return requests.post(f"http://{req.remote_addr}:5000/receive", data="404")
+
+if __name__ == '__main__':
+    listener(5001)
